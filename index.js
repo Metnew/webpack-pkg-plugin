77c7bd8edb28ef5f15870fb129368799e6f57974
Christoph Mayr <christoph.mayr@moldev.com>
2019-10-11 11:21:49 +0200

    feat: add scripts and assets option

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const { exec } = require('pkg');
 const { join } = require('path');
+const { writeFileSync, unlinkSync } = require('fs');
 
 class WebpackPkgPlugin {
-  constructor({ targets, output }) {
+  constructor({ targets = ['host'], output, assets = [], scripts = [] }) {
     const isArray = targets instanceof Array;
     if (!isArray) {
       console.log(
-        '"targets" option for WebpackPkgPlugin isn\'t specified. Using defaults.'
+        '"targets" option for WebpackPkgPlugin has the wrong format. Using defaults (["host"]).'
       )
     }
 
-    const arrayOfTargets = isArray ? targets : ['host'];
     this.options = {
-      targets: arrayOfTargets,
-      output
+      targets,
+      output,
+      scripts,
+      assets
     }
   }
 
   apply(compiler) {
     compiler.hooks.afterEmit.tapAsync('WebpackPkgPlugin', async (compilation, callback) => {
-      const { targets, output } = this.options;
+      const { targets, output, scripts, assets } = this.options;
       const outputPath = compilation.compiler.options.output.path;
 
       const files = Object.keys(compilation.assets).filter((_, i) => i < Object.keys(compilation.compiler.options.entry).length);
@@ -27,7 +29,23 @@ class WebpackPkgPlugin {
       for (const file of files) {
         const entry = join(outputPath, file);
         const distPath = join(outputPath, output);
-        await exec([entry, '--targets', targets.join(','), '--out-path', distPath]);
+
+        if (assets.length || scripts.length) {
+          const configJsonPath = `${ outputPath }/pkg-config.json`;
+          writeFileSync(configJsonPath, JSON.stringify({
+            targets,
+            assets,
+            scripts
+          }));
+
+          try {
+            await exec([entry, '--config', configJsonPath, '--out-path', distPath]);
+          } catch {
+            unlinkSync(configJsonPath);
+          }
+
+          unlinkSync(configJsonPath);
+        } else await exec([entry, '--targets', targets.join(','), '--out-path', distPath]);
       }
 
       callback();
